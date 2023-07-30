3b2d0286cbf5ee33c3db6967e0f77c2c161e2f90
Ethan Lynch <ethanlynch72@gmail.com>
2023-07-30 01:45:07 -0400

    edited app.js to change from components to views

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import DisplayAll from './components/DisplayAll';
-import OneRoutine from './components/OneRoutine';
-import UpdateRoutine from './components/UpdateRoutine';
-import RoutineForm from './components/RoutineForm';
-import Login from './components/Login';
-import Signup from './components/Signup';
+// import DisplayAll from './components/DisplayAll';
+// import OneRoutine from './components/OneRoutine';
+// import UpdateRoutine from './components/UpdateRoutine';
+// import RoutineForm from './components/RoutineForm';
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+import { Home, Exercises, CreateRoutine, CreateExercise, Update, Login, Signup} from './pages';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; // Import custom CSS file for styling
 
@@ -15,12 +16,11 @@ function App() {
       
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DisplayAll />} />
-          <Route path="/routines/:id" element={<OneRoutine />} />
-          <Route path="/update/:id" element={<UpdateRoutine />} />
-          <Route path="/new" element={<RoutineForm />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/exercises" element={<Exercises />} />
+          <Route path="/create" element={<CreateRoutine />} />
+          <Route path="/create-exercise" element={<CreateExercise />} />
+          <Route path="/update/:id" element={<Update />} />
         </Routes>
       </BrowserRouter>
     </div>
